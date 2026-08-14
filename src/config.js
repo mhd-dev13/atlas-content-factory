b72d102d638430d9f4fcbc8b6a24d5d0f03a355c
mhd-dev13 <rezaimohammad347@gmail.com>
2026-08-14 11:38:41 +0330

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,11 @@
+export const CONFIG = {
+  botName: "Atlas",
+  contentChannel: "@AtlasContentFactory",
+
+  model: "gpt-5-mini",
+
+  maxTokens: 1000,
+  temperature: 0.7,
+
+  version: "2.0.0"
+};
